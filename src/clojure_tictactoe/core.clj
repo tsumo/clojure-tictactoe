00152c7485f9ef2e7d0ed2f62947a7fb1c1c2407
tsumo <tsumo@users.noreply.github.com>
2019-02-19 17:57:37 +0300

    switch to the zero-based numbering

--- src/clojure_tictactoe/core.clj
+++ src/clojure_tictactoe/core.clj
@@ -1,48 +1,48 @@
 (ns clojure-tictactoe.core
   (:gen-class))
 
-;;; {1 [[1 2 3] [1 4 7]]
-;;;  2 [[1 2 3] [2 5 8]]
-;;;  3 [[1 2 3] [3 6 9]]
-;;;  4 [[4 5 6] [1 4 7]]
-;;;  5 [[4 5 6] [2 5 8]]
-;;;  6 [[4 5 6] [3 6 9]]
-;;;  7 [[7 8 9] [1 4 7]]
-;;;  8 [[7 8 9] [2 5 8]]
-;;;  9 [[7 8 9] [3 6 9]]}
+;;; {0 [[0 1 2] [0 3 6] [0 4 8]
+;;;  1 [[0 1 2] [1 4 7]]
+;;;  2 [[0 1 2] [2 5 8] [2 4 6]
+;;;  3 [[3 4 5] [0 3 6]]
+;;;  4 [[3 4 5] [1 4 7] [0 4 8] [2 4 6]
+;;;  5 [[3 4 5] [2 5 8]]
+;;;  6 [[6 7 8] [0 3 6] [2 4 6]
+;;;  7 [[6 7 8] [1 4 7]]
+;;;  8 [[6 7 8] [2 5 8] [0 4 8]}
 
-;;; 1 2 3
-;;; 4 5 6
-;;; 7 8 9
+;;; 0 1 2
+;;; 3 4 5
+;;; 6 7 8
 
-;;;  1  2  3  4
-;;;  5  6  7  8
-;;;  9 10 11 12
-;;; 13 14 15 16
+;;;  0  1  2  3
+;;;  4  5  6  7
+;;;  8  9 10 11
+;;; 12 13 14 15
 
-;;;  1  2  3  4  5
-;;;  6  7  8  9 10
-;;; 11 12 13 14 15
-;;; 16 17 18 19 20
-;;; 21 22 23 24 25
+;;;  0  1  2  3  4
+;;;  5  6  7  8  9
+;;; 10 11 12 13 14
+;;; 15 16 17 18 19
+;;; 20 21 22 23 24
 
 (defn get-row-num
   "Returns row on which cell is located."
   [cell size]
-  (inc (quot (dec cell) size)))
+  (quot cell size))
 
 
 (defn get-col-num
   "Return col on which cell is located."
   [cell size]
   (let [row-num (get-row-num cell size)]
-    (- cell (- (* size row-num) size))))
+    (- cell (* size row-num))))
 
 
 (defn get-row
   "Nth row of the board."
   [row size]
-  (let [start (inc (* (dec row) size))]
+  (let [start (* row size)]
     (range start (+ start size))))
 
 
--- test/clojure_tictactoe/core_test.clj
+++ test/clojure_tictactoe/core_test.clj
@@ -4,14 +4,28 @@
 
 (deftest board-tests
   (testing "Testing board getters"
-    (is (= (get-row 3 4) '(9 10 11 12)))
-    (is (= (get-col 4 5) '(4 9 14 19 24)))
-    (is (= (map get-row-num (range 1 10) (repeat 3))
-           '(1 1 1 2 2 2 3 3 3)))
-    (is (= (map get-row-num (range 1 17) (repeat 4))
-           '(1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4)))
-    (is (= (map get-col-num (range 1 10) (repeat 3))
-           '(1 2 3 1 2 3 1 2 3)))
-    (is (= (map get-col-num (range 1 17) (repeat 4))
-           '(1 2 3 4 1 2 3 4 1 2 3 4 1 2 3 4)))))
+
+    (is (= (map get-row (range 3) (repeat 3))
+           '((0 1 2) (3 4 5) (6 7 8)))
+        "Getting every row of the 3x3 board.")
+
+    (is (= (map get-col (range 3) (repeat 3))
+           '((0 3 6) (1 4 7) (2 5 8)))
+        "Getting every col of the 3x3 board.")
+
+    (is (= (map get-row-num (range 9) (repeat 3))
+           '(0 0 0 1 1 1 2 2 2))
+        "Getting row number for every cell of the 3x3 board.")
+
+    (is (= (map get-row-num (range 16) (repeat 4))
+           '(0 0 0 0 1 1 1 1 2 2 2 2 3 3 3 3))
+        "Getting row number for every cell of the 4x4 board.")
+
+    (is (= (map get-col-num (range 9) (repeat 3))
+           '(0 1 2 0 1 2 0 1 2))
+        "Getting col number for every cell of the 3x3 board.")
+
+    (is (= (map get-col-num (range 16) (repeat 4))
+           '(0 1 2 3 0 1 2 3 0 1 2 3 0 1 2 3))
+        "Getting col number for every cell of the 4x4 board.")))
 
